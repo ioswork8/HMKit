9ec027503c0d157ea48365379edf1f49a24d1901
ioswork8 <dev@ioswork8.com>
2015-04-03 15:46:29 +0800

    no message

---
+++ HMKit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  HMKit
+//
+//  Created by hvming on 15/4/3.
+//  Copyright (c) 2015年 ioswork8. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ HMKit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  HMKit
+//
+//  Created by hvming on 15/4/3.
+//  Copyright (c) 2015年 ioswork8. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
